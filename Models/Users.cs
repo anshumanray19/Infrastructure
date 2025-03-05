a0c5397ebd8566e69a8f10454f15fd0815506278
Anshuman <anshuman@coolsoft-tech.com>
2025-03-05 17:12:10 +0530

    Models

---
+++ Models/Users.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Models
+{
+    public class Users
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public int RoleID { get; set; }
+        //public Role Role { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        //public ICollection<UserRole> UserRoles { get; set; }
+        public bool EmailConfirmed { get; set; } = false;
+        public DateTime? LastLogin { get; set; }
+    }
+}
