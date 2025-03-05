53ffc44aec0e8b31a62345a4b09c5889258157ac
Anshuman <anshuman@coolsoft-tech.com>
2025-03-05 16:43:21 +0530

    Project Setup With DB

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+
+namespace Infrastructure.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+// Add Database Context
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -9,7 +17,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -20,6 +27,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Map Controllers (Important)
+app.MapControllers();  // This ensures your /home/index route works
+
+// Map Razor Pages
 app.MapRazorPages();
+
 
 app.Run();
